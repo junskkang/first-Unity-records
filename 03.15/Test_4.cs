1aebf87eb6b5151fa553900f3100b20763b7cbce
junskkang <junskk6512@gmail.com>
2024-03-18 14:41:36 +0900

    Update Test_4.cs

--- 03.15/Test_4.cs
+++ 03.15/Test_4.cs
@@ -11,12 +11,14 @@ public class Test_4 : MonoBehaviour
         // 인덱스를 찾는 코드를 작성하고 인덱스를 출력해 주세요.
         int[] App = { 34, 56, 12, 89, 120, 6, 8, 222, 67, 81, 110 };
 
+
+        //답
         int aa = 0;
         while (App[aa] != 222)
         {
             aa++;
         }
-        Debug.Log("해당 배열의 222값은 " + aa + "번째 인덱스에 있습니다.");
+        Debug.Log("해당 배열의 222값은 " + (aa + 1) + "번째 인덱스에 있습니다.");
 
         //2. 아래와 같은 배열이 있을 때 33이라는 값은 몇 번 인덱스에 있을까요?
         //또, 99라는 값은 몇 번째 인덱스에 있을까요?
@@ -32,29 +34,31 @@ public class Test_4 : MonoBehaviour
             }
         }
 
+
+        //답
         //while문
         int find33 = 0;
         while (VVV[find33] != 33)
         {
             find33++;
         }
-        Debug.Log("해당 배열의 33이라는 값은 " + find33 + "번째 인덱스에 있습니다.");
+        Debug.Log("해당 배열의 33이라는 값은 " + (find33 + 1) + "번째 인덱스에 있습니다.");
         
         int find99 = 0;
         while (VVV[find99] != 99)
         {
             find99++;
         }
-        Debug.Log("해당 배열의 99이라는 값은 " + find99 + "번째 인덱스에 있습니다.");
+        Debug.Log("해당 배열의 99이라는 값은 " + (find99 + 1) + "번째 인덱스에 있습니다.");
 
 
         //for문
         for(int find = 0; find <= a_idx; find++ )
         {
             if (VVV[find] == 33)
-                Debug.Log("해당 배열의 33이라는 값은 " + find + "번째 인덱스에 있습니다.");
+                Debug.Log("해당 배열의 33이라는 값은 " + (find + 1) + "번째 인덱스에 있습니다.");
             else if (VVV[find] == 99)
-                Debug.Log("해당 배열의 99이라는 값은 " + find + "번째 인덱스에 있습니다.");
+                Debug.Log("해당 배열의 99이라는 값은 " + (find + 1) + "번째 인덱스에 있습니다.");
         }
 
 
@@ -76,6 +80,8 @@ public class Test_4 : MonoBehaviour
         //          :
         //a_rr[?] = ZZZ[98] + ZZZ[99];
 
+
+        //답
         int yy = 0;
         int zz = 0;
         
@@ -83,11 +89,12 @@ public class Test_4 : MonoBehaviour
         {
             a_rr[yy] = ZZZ[zz] + ZZZ[zz + 1];
             Debug.Log("a_rr[" + yy + "] : " + a_rr[yy]);
-            yy++;
-            zz+=2;
-           
+
             if (zz == 98)
                 break;
+
+            yy++;
+            zz+=2;
         }    
 
 
@@ -96,6 +103,37 @@ public class Test_4 : MonoBehaviour
         int[] a_kk = { 23, 45, 12, 67, 34, 77, 103, 3, 6, 7, 8, 11, 65, 204, 33, 56 };
         //for문을 돌면서 최대값과 최솟값을 구하고 최대값의 인덱스와 최소값의 인덱스를 같이 출력해 주세요.
         //출력 예시 "최댓값 : ?? (인덱스 ??), 최솟값 : ?? (인덱스 ??)"
+
+        //답
+        int min = a_kk[0];
+        int max = a_kk[0];
+
+        for (int count = 0; count < a_kk.Length; count++)
+        {
+            if (a_kk[count] < min)
+                min = a_kk[count];
+            else if (a_kk[count] > max)
+                max = a_kk[count];
+        }
+
+        int xx = 0;
+        int idxMin = 0;
+        int idxMax = 0;
+
+        while (true)
+        {
+            if (xx == a_kk.Length)
+                break;
+            else if (a_kk[xx] == min)
+                idxMin = xx+1;
+            else if (a_kk[xx] == max)
+                idxMax = xx+1;
+
+            xx++;
+        }
+        
+        Debug.Log("최솟값 : " + min + "(" + idxMin + ")번째 인덱스, 최댓값 : " + max + "(" + idxMax + "번째 인덱스)");
+
 
     }
 
