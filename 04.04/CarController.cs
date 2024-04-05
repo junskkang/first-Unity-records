3951abdc453b55ed059c649c970b8ede73dc1a7c
junskkang <junskk6512@gmail.com>
2024-04-05 18:35:35 +0900

    SwipeCar

--- 04.04/CarController.cs
+++ 04.04/CarController.cs
@@ -8,9 +8,9 @@ public class CarController : MonoBehaviour
     float speed = 0;
     Vector2 startPos;
 
-    bool IsMove = false;
+    //bool IsMove = false;  //자동차의 상태 구분
 
-    GameDirector m_GameD = null;
+    GameDirector m_GameD = null;  //게임디렉터 스크립트를 찾아오기 위한 변수
 
     void Start()
     {
@@ -19,6 +19,12 @@ public class CarController : MonoBehaviour
         //모니터 주사율이 다른 컴퓨터일 경우 캐릭터 조작시 빠르게 움직일 수 있다.
 
         m_GameD = GameObject.FindObjectOfType<GameDirector>();
+
+        ////게임디렉터 찾는 정석 방법
+        //GameObject a_Obj = GameObject.Find("GameDirector"); //하이어라키에 있는 오브젝트 불러옴
+        //if(a_Obj != null )
+        //    m_GameD = a_Obj.GetComponent<GameDirector>(); 
+        ////오브젝트에 붙어있는 게임디렉터 컴포넌트(스크립트)를 불러옴
     }
 
     void Update()
@@ -32,7 +38,8 @@ public class CarController : MonoBehaviour
             return;
 
         //스와이프 한 만큼 속도 부여
-        if (IsMove == false)
+        //if (IsMove == false) //자동차가 멈춰있을 때만 조작 가능하도록
+        if(GameDirector.s_State == GameState.Ready)
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -50,31 +57,37 @@ public class CarController : MonoBehaviour
                     float swipeLength = endPos.x - startPos.x;
                     //스와이프한 길이만큼 속도 부여
                     this.speed = swipeLength / 500.0f;   //500은 저자가 정한 상수값
-                    
+
                     //효과음 재생
                     GetComponent<AudioSource>().Play();
 
-                    IsMove = true;
+                    m_GameD.RecordTimer = 2.0f;
+                        
+                    GameDirector.s_State = GameState.Move;
                 }
             }
         }
+        //else if (IsMove == true)  //자동차가 움직이는 상태
+        else if (GameDirector.s_State == GameState.Move)
+        {
+            transform.Translate(this.speed, 0, 0); //속도만큼 x축으로 이동
+                                                   //transform.position += new Vector3(this.speed, 0, 0);  위에와 같은 의미
+            this.speed *= 0.98f;                   //감속
 
-
-        transform.Translate(this.speed, 0, 0); //속도만큼 x축으로 이동
-        //transform.position += new Vector3(this.speed, 0, 0);  위에와 같은 의미
-        this.speed *= 0.98f;                   //감속
-
-        if (IsMove == true) 
-        { 
-            if (this.speed <= 0.1f)  //이쯤이면 정지 판정
+            if (this.speed <= 0.005f)  //이쯤이면 정지 판정
             {
                 this.speed = 0;
 
-                if (m_GameD != null)
+                m_GameD.RecordTimer -= Time.deltaTime;  //2초 후 기록 저장
+                if (m_GameD.RecordTimer <= 0)
+                    m_GameD.RecordTimer = 0;
+
+                if (m_GameD != null && m_GameD.RecordTimer == 0) 
                 {
-                    m_GameD.Record();
-                    transform.position = new Vector3(-7, -3.7f, 0);
-                    IsMove = false;
+                    m_GameD.Record();  //이번 플레이어의 기록 저장
+                    transform.position = new Vector3(-7, -3.7f, 0);  //자동차 위치 초기화
+                    //IsMove = false;  //자동차 상태 변경
+                    GameDirector.s_State = GameState.Ready;
                 }
             }
         }
--- 04.04/GameDirector.cs
+++ 04.04/GameDirector.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public enum GameState
+{
+    Ready = 0,     //플레이어 출발선에서 준비 중
+    Move = 1,      //자동차가 움직이고 있는 상태
+    GameEnd = 2    //플레이어 3명의 플레이가 모두 끝난 상태
+}
+
+public class PlayerData
+{
+    public int m_Index = 0;          //플레이어 번호
+    public float m_SvLength = 0.0f;  //플레이어별 기록 저장
+    public int m_Ranking = -1;       //랭킹 변수 -1은 아직 순위가 매겨지지 않았다는 의미
+}
 
 public class GameDirector : MonoBehaviour
 {
+    public static GameState s_State = GameState.Ready;  //어디서나 쉽게 접근할 수 있게 static선언
+
     GameObject car;
     GameObject flag;
     GameObject distance;
 
-    public Text[] Player;
+    public Text[] Player;  //플레이어별 기록 Text UI
     [HideInInspector] public int PlayerNum;
+    // PlayerNum 0 = Player1 , PN 1 = Player2 , PN 2 = Player3
+
+    public float RecordTimer = 0.0f;
 
     public Button Replay_Btn;
 
-    [HideInInspector] public float RecordLength;
-    List<float> PlayerRecord = new List<float>();
-    List<float> PlayerScore = new List<float>();
+    [HideInInspector] public float RecordLength;   //지금 플레이 중인 유저의 거리 저장용 변수
+    List<float> PlayerRecord = new List<float>();  //기록 저장용 리스트
+    List<float> PlayerScore = new List<float>();   //기록 순위 정렬용 리스트
 
+    //각 플레이어 별로 깃발까지의 거리를 저장하기 위한 리스트 변수
+    List<PlayerData> m_PlayerList = new List<PlayerData>();
 
     void Start()
-    {  
+    {
+        s_State = GameState.Ready;  //static변수는 시작할 때 한번씩 초기화 해주도록
+
         if (Replay_Btn != null)
             Replay_Btn.onClick.AddListener(() =>
             { 
-                
                 SceneManager.LoadScene("GameScene");
             });
         
@@ -40,9 +61,12 @@ public class GameDirector : MonoBehaviour
     {
         float length = (this.flag.transform.position.x - this.car.transform.position.x);
         float AbLength = Mathf.Abs(length);
+        //절대값
+        //if( length < 0)
+        //    length = -length;
 
         this.distance.GetComponent<Text>().text = "Distance : " + AbLength.ToString("F2") + " m";
-        RecordLength = AbLength;
+        RecordLength = AbLength; //기록값을 멤버 변수로 저장
     }
 
     public void Record()
@@ -51,9 +75,18 @@ public class GameDirector : MonoBehaviour
         PlayerNum++;
         PlayerRecord.Add(RecordLength);
         PlayerScore.Add(RecordLength);
+        
+        PlayerData a_Node = new PlayerData();
+        a_Node.m_Index = PlayerNum;
+        a_Node.m_SvLength = RecordLength;
+        m_PlayerList.Add(a_Node);
 
-        if (PlayerScore.Count == 3)
-            Judge();
+        if (PlayerScore.Count == 3) //모든 유저가 플레이를 끝낸 경우
+        {
+            s_State = GameState.GameEnd;
+            Judge(); //순위 판정
+        }
+            
     }
 
     void Judge()
@@ -75,6 +108,28 @@ public class GameDirector : MonoBehaviour
                 this.Player[i].GetComponent<Text>().text += " 3등";
             }
         }
-        Replay_Btn.gameObject.SetActive(true);
+        Replay_Btn.gameObject.SetActive(true); //리플레이 버튼 활성화
+
+        ////깃발까지의 거리(m_PlayerList[0].m_SvLength)를 기준으로 오름차순 정렬(ASC)
+        //m_PlayerList.Sort(SvLenComp);
+
+        ////정리해서 출력
+        //PlayerData a_Player = null;
+        //for(int i = 0; i < m_PlayerList.Count; i++)
+        //{
+        //    a_Player = m_PlayerList[i];
+
+        //    if (Player.Length <= a_Player.m_Index)
+        //        continue;
+
+        //    a_Player.m_Ranking = i + 1;
+
+        //    Player[a_Player.m_Index].text += " " + a_Player.m_Ranking + "등";
+        //}
     }
+    
+    //int SvLenComp(PlayerData a, PlayerData b)
+    //{
+    //    return a.m_SvLength.CompareTo(b.m_SvLength);
+    //}
 }
